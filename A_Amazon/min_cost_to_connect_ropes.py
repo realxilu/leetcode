0ed857e7c0580d571d7967f91d100fa044125bbf
realxilu <69646469+realxilu@users.noreply.github.com>
2021-04-22 08:08:00 -0700

    Update min_cost_to_connect_ropes.py

--- A_Amazon/min_cost_to_connect_ropes.py
+++ A_Amazon/min_cost_to_connect_ropes.py
@@ -20,8 +20,8 @@ def connect_rope(arr):
     return sum_
 
 
-# list_ = connect_rope([8, 4, 6, 12]) # 58
+list_ = connect_rope([8, 4, 6, 12]) # 58
 # list_ = connect_rope([20, 4, 8, 2]) # 54
 # list_ = connect_rope([1, 2, 5, 10, 35, 89]) # 224
-list_ = connect_rope([2, 2, 3, 3])  # 20
+# list_ = connect_rope([2, 2, 3, 3])  # 20
 print(list_)
