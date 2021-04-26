87d27392acc60524b16c4865797855f188905972
realxilu <69646469+realxilu@users.noreply.github.com>
2021-04-26 00:27:25 -0700

    t

---
+++ educative/two pointer/pair_with_target_sum.py
@@ -0,0 +1,13 @@
+def pair_with_targetsum(arr, target_sum):
+  i, j = 0, len(arr) - 1
+
+  while i < j:
+    if arr[i] + arr[j] == target_sum:
+      return [i, j]
+
+    if arr[i] + arr[j] > target_sum:
+      j -= 1
+    else:
+      i += 1
+
+  return -1
