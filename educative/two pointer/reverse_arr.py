b4c4bf8a4f085a1c115305a1e96b692c08c2722d
realxilu <69646469+realxilu@users.noreply.github.com>
2021-04-25 20:07:28 -0700

    Create reverse_arr.py

---
+++ educative/two pointer/reverse_arr.py
@@ -0,0 +1,13 @@
+# [in-place] [two-pointer]
+def reverse_arr(arr):
+    i, j = 0, len(arr) - 1
+
+    while i < j:
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+        j -= 1
+
+    return arr
+
+arr = [1,2,3,4]
+print(reverse_arr(arr))
