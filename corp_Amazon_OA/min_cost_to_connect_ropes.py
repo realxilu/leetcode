9876640f18a7e6119a44ab1b747bb3d0d0883f35
realxilu <69646469+realxilu@users.noreply.github.com>
2021-04-27 11:20:11 -0700

    Update min_cost_to_connect_ropes.py

--- corp_Amazon_OA/min_cost_to_connect_ropes.py
+++ corp_Amazon_OA/min_cost_to_connect_ropes.py
@@ -1,24 +1,24 @@
 from heapq import *
-# [PRIORITY QUEUE]
+# [PRIORITY-QUEUE]
 
 def connect_rope(arr):
-    sum_ = 0
+    _sum = 0
     heapify(arr)
     while arr:
         try:
             a = heappop(arr)
             b = heappop(arr)
             c = a + b
-            sum_ += c
+            _sum += c
             heappush(arr, c)
         except:
             pass
 
-    return sum_
+    return _sum
 
-
-list_ = connect_rope([8, 4, 6, 12]) # 58
-# list_ = connect_rope([20, 4, 8, 2]) # 54
-# list_ = connect_rope([1, 2, 5, 10, 35, 89]) # 224
-# list_ = connect_rope([2, 2, 3, 3])  # 20
-print(list_)
+# [MAIN]
+l = connect_rope([8, 4, 6, 12]) # 58
+# l = connect_rope([20, 4, 8, 2]) # 54
+# l = connect_rope([1, 2, 5, 10, 35, 89]) # 224
+# l = connect_rope([2, 2, 3, 3])  # 20
+print(l)
