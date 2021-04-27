6a811a615408a5f49e8550e3e80b7a5f18b7c48d
realxilu <69646469+realxilu@users.noreply.github.com>
2021-04-27 11:12:20 -0700

    Update valid_paren.py

--- leetcode_common/valid_paren.py
+++ leetcode_common/valid_paren.py
@@ -13,3 +13,25 @@ class Solution:
                 return False
 
         return len(stack) == 0
+
+# TODO refactor
+# const isValid = (string) = > {
+#     let stack = []
+#     // define parentheses pairs
+#     let dict = {
+#         '{': '}',
+#         '[': ']',
+#         '(': ')',
+#     }
+
+#     for (let c of string) {
+#         if (c == = '{' | | c == = '[' | | c == = '(')
+#           stack.push(c)
+#         else if (c !== dict[stack[stack.length - 1]])
+#           return false
+#         else
+#           stack.pop()
+#     }
+
+#     return stack.length === 0
+# }
