d6cd79ab9ec8cdb07f8df4f06d8e0464f049ba56
realxilu <69646469+realxilu@users.noreply.github.com>
2021-04-26 22:08:31 -0700

    Update max_sub_array_size_k.py

--- educative/sliding window/max_sub_array_size_k.py
+++ educative/sliding window/max_sub_array_size_k.py
@@ -1,17 +1,18 @@
 import math
 
-
+# i: start
+# j: end
 def max_sub_array_of_size_k(k, arr):
-  _sum, _max, i, j = 0, -math.inf, 0, 0
+    _sum, _max, i, j = 0, -math.inf, 0, 0
 
-  while (j < len(arr)):
-    _sum += arr[j]
+    while j < len(arr):
+        _sum += arr[j]
 
-    if (j >= k - 1):
-      _max = max(_max, _sum)
-      _sum -= arr[i]
-      i += 1
+        if (j >= k - 1):
+            _max = max(_max, _sum)
+            _sum -= arr[i]
+            i += 1
 
-    j += 1
+        j += 1
 
-  return _max
+    return _max
