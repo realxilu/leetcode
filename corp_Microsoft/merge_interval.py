eb501153db0d653f5fb53eb115c905e170d59370
realxilu <69646469+realxilu@users.noreply.github.com>
2021-04-27 23:27:04 -0700

    Update merge_interval.py

--- corp_Microsoft/merge_interval.py
+++ corp_Microsoft/merge_interval.py
@@ -1,22 +1,21 @@
 class Solution:
     def merge(self, intervals):
-        # [KEY] sorted by the starting indices
+        # [IMPORTANT] sorted by the starting indices
         intervals = sorted(intervals, key=lambda x: x[0])
 
-        # [TIP] pre is a sentry/dummy
+        # [innovative] usage of dummy
         res, pre = [], intervals[0]
 
-        # [innovative] [1:] means start from the 2nd element(indexed as 1)
+        # intervals[1:] -> start from the 2nd element
         for x in intervals[1:]:
-            # previous(pre) end is great than or touching the current(x) start
-            # pre[1] = pre's end; x[0] = x's start
+            # previous end is great than or touching the next interval's start
             if pre[1] >= x[0]:
                 pre[1] = max(pre[1], x[1])
             else:
                 res.append(pre)
                 pre = x
 
-        # append the last interval
+        # don't forget the last case
         res.append(pre)
 
         return res
