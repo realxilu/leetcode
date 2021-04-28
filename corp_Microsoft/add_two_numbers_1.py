033bef78758a9374e921719a052b3f6a5b9c06b1
realxilu <69646469+realxilu@users.noreply.github.com>
2021-04-27 23:17:32 -0700

    Update add_two_numbers_1.py

--- corp_Microsoft/add_two_numbers_1.py
+++ corp_Microsoft/add_two_numbers_1.py
@@ -1,24 +1,29 @@
 class Solution:
-    def add_two_numbers(self, l1, l2):
+    def addTwoNumbers(self, l1, l2):
         cur1, cur2 = l1, l2
+        # [innovative][sentry] the use of sentry will make the while clean and easy to read
         cur = dummy = ListNode(-1)
-        sum_ = 0
+        _sum = 0
 
         while cur1 or cur2:
-            sum_ = sum_ // 10
+            # example 14 // 10 = 1 (carry)
+            # example 0  // 10 = 0
+            _sum = _sum // 10
 
             if cur1:
-                sum_ += cur1.val
+                _sum += cur1.val
                 cur1 = cur1.next
 
             if cur2:
-                sum_ += cur2.val
+                _sum += cur2.val
                 cur2 = cur2.next
 
-            cur.next = ListNode(sum_ % 10)
+            # example 14 % 10 = 4
+            cur.next = ListNode(_sum % 10)
             cur = cur.next
 
-        if sum_ >= 10:
+        # don't forget about the last digit
+        if _sum >= 10:
             cur.next = ListNode(1)
 
         return dummy.next
