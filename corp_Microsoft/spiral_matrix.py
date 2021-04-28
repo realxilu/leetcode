68a3630e8d7d8b2c6ae95d6c6a28144e481e6cc4
realxilu <69646469+realxilu@users.noreply.github.com>
2021-04-27 22:57:21 -0700

    Update spiral_matrix.py

--- corp_Microsoft/spiral_matrix.py
+++ corp_Microsoft/spiral_matrix.py
@@ -1,3 +1,5 @@
+#[KEY] maintain boundary indices that maps the outer layer of the box
+# notice that row_i, row_j, col_i and col_j are indices/pointers not the length
 class Solution:
     def spiralOrder(self, matrix):
         row_i, row_j = 0, len(matrix) - 1
@@ -5,26 +7,29 @@ class Solution:
         res = []
 
         while True:
-            for x in range(col_i, col_j + 1):
-                res.append(matrix[row_i][x])
+            # note col_j + 1 'range'
+            for j in range(col_i, col_j + 1):
+                res.append(matrix[row_i][j])
             row_i += 1
+            # while true + if break is awesome!
             if row_i > row_j:
                 break
 
-            for x in range(row_i, row_j + 1):
-                res.append(matrix[x][col_j])
+            for i in range(row_i, row_j + 1):
+                res.append(matrix[i][col_j])
             col_j -= 1
             if col_i > col_j:
                 break
 
-            for x in range(col_j, col_i - 1, -1):
-                res.append(matrix[row_j][x])
+            # notice the 3rd argument is -1
+            for j in range(col_j, col_i - 1, -1):
+                res.append(matrix[row_j][j])
             row_j -= 1
             if row_i > row_j:
                 break
 
-            for x in range(row_j, row_i - 1, -1):
-                res.append(matrix[x][col_i])
+            for i in range(row_j, row_i - 1, -1):
+                res.append(matrix[i][col_i])
             col_i += 1
             if col_i > col_j:
                 break
