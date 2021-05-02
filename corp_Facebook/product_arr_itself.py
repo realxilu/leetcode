434531ad789e7f62e17abbae74e555f4ee1b142f
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-02 15:57:39 -0700

    Create product_arr_itself.py

---
+++ corp_Facebook/product_arr_itself.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def productExceptSelf(self, nums):
+        if not nums:
+            return None
+
+        tmp = 1
+        res = len(nums) * [1]
+
+        for i in range(len(nums) - 1):
+            tmp *= nums[i]
+            res[i + 1] = tmp
+
+        tmp = 1
+        for i in range(len(nums) - 1, 0, -1):
+            tmp *= nums[i]
+            res[i - 1] = tmp * res[i - 1]
+
+        return res
