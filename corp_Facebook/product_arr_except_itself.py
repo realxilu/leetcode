2d30a52ffad4e1e1b4d0bfaf40b1e0e5ee7685b1
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-02 23:03:58 -0700

    t

--- corp_Facebook/product_arr_except_itself.py
+++ corp_Facebook/product_arr_except_itself.py
@@ -2,16 +2,19 @@ class Solution:
     def productExceptSelf(self, nums):
         if not nums:
             return None
-
+        
+        n = len(nums)
         tmp = 1
-        res = len(nums) * [1]
+        res = [1] * n
 
-        for i in range(len(nums) - 1):
+        # [0, 1 ... n - 2]
+        for i in range(n - 1):
             tmp *= nums[i]
             res[i + 1] = tmp
 
         tmp = 1
-        for i in range(len(nums) - 1, 0, -1):
+        # [n - 1, n - 2, ... 1]
+        for i in range(n - 1, 0, -1):
             tmp *= nums[i]
             res[i - 1] = tmp * res[i - 1]
 
