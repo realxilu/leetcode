be0d539a6218ad782fbecf954ce440c2c1eda0ab
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-03 00:30:23 -0700

    Update basic_calculator_1.py

--- corp_Microsoft/basic_calculator_1.py
+++ corp_Microsoft/basic_calculator_1.py
@@ -1,17 +1,20 @@
 class Solution:
-    # [KEY] process the previous number when hitting the current sign
     def calculate(self, s):
-        res, num, sign, stack = 0, 0, 1, []
+        res = 0
+        stack = []
+        sign, num = 1, 0
 
         for c in s:
+            # ignore space
             if (c == ' '):
                 continue
 
             if c.isdigit():
+                # [TRICK] build the number using digits
                 num = 10 * num + int(c)
             elif c == '+':
-                res += sign * num
-                # after using 'sign' and 'num', reset them
+                res += sign * num # this is for the previous* operation
+                # the reset is for the next operation
                 num = 0
                 sign = 1
             elif c == '-':
@@ -22,7 +25,6 @@ class Solution:
                 stack.append(res)
                 stack.append(sign)
                 sign = 1
-                # resetting 'res' to perform in-paren calculation
                 res = 0
             elif c == ')':
                 # perform in-paren calculation
@@ -31,7 +33,12 @@ class Solution:
                 res *= stack.pop()
                 res += stack.pop()
 
-        # handle the last number cuz' there is no sign after the last number
+        # [ATTENTION] handle the last number cuz' there is no sign after the last number
         res += sign * num
 
         return res
+
+ # [KEY] 1 + 2 - 3
+ # whenever an operator is hit, it means a number must have been formed.
+ # res += sign * num is for the prevoius operation
+ # what is the previous operation when the first '+' was hit, it is res = 0 (init) + 1
