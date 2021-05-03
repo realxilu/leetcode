e1404a7e9e630fa1d1090f933ceb175b073d06d6
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-02 23:47:05 -0700

    Update copy_linkedlist_with_rand_pointer.py

--- corp_Microsoft/copy_linkedlist_with_rand_pointer.py
+++ corp_Microsoft/copy_linkedlist_with_rand_pointer.py
@@ -11,12 +11,12 @@ class Solution:
         # second traversal sets up the proper relationship between nodes
         cur = head
         while cur:
+            # dic.get('stuff') is safer than dic['stuff']
             dic.get(cur).next = dic.get(cur.next)
             dic.get(cur).random = dic.get(cur.random)
             cur = cur.next
 
         return dic.get(head)
-
 
 class Node:
     def __init__(self, val, next, random):
