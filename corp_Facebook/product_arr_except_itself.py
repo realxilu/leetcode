f62bde00d32e5e3669efeff271006be09b0ad670
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-02 23:27:11 -0700

    Update product_arr_except_itself.py

--- corp_Facebook/product_arr_except_itself.py
+++ corp_Facebook/product_arr_except_itself.py
@@ -7,13 +7,13 @@ class Solution:
         tmp = 1
         res = [1] * n
 
-        # 0 -> n - 2
+        # 0 to n - 2 inclusive (0-based)
         for i in range(n - 1):
             tmp *= nums[i]
             res[i + 1] = tmp
 
         tmp = 1
-        # n - 1 -> 1
+        # n - 1 to 1 inclusive (0-based)
         for i in range(n - 1, 0, -1):
             tmp *= nums[i]
             res[i - 1] = tmp * res[i - 1] # this time we cross multiply the previously generated res array
