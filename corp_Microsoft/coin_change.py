17eba198fbf935dff4d8f9509c1d452d48265a56
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-03 18:12:04 -0700

    Update coin_change.py

--- corp_Microsoft/coin_change.py
+++ corp_Microsoft/coin_change.py
@@ -2,19 +2,22 @@
 
 # Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, 
 
-# return -1.
+# return -1. You may assume that you have an infinite number of each kind of coin.
 
-# You may assume that you have an infinite number of each kind of coin.
+# ----------------------------------------------------------------------------------------------------
+
+# x = current amount, for example 1 cent 2 cents .. 17 cents
+# i = coin denomiation index
+
 class Solution:
     # [BEST]
     def coinChange(self, coins, amount):
-        n = len(coins)
-        dp = [0] + [float('inf')] * amount
+        dp = [0] + [float('inf')] * amount # [0] is the base case, it takes 0 coin to change 0 coin
 
         # build table bottom up: $1, $2, $3 ... $x
         for x in range(1, amount + 1):
             # check each denomination
-            for i in range(n):
+            for i in range(len(coins)):
                 if coins[i] <= x:
                     # either use or not use the coin
                     dp[x] = min(dp[x], dp[x - coins[i]] + 1)
