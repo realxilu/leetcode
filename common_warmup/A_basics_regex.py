dbf59058ffc7ac96697f04f1737ce57e63f35665
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-04 12:19:53 -0700

    t

--- common_warmup/A_basics_regex.py
+++ common_warmup/A_basics_regex.py
@@ -1,9 +1,12 @@
-# regex
+# https://docs.python.org/3/library/re.html
+
 import re
 
 # substitution
-#
 
 my_string = 'The WORLD we live, in is RatheR hoStile!** BE AWARE @#$*('
 x = re.sub('\W+', ' ', my_string)
 print(x)
+
+# matching TODO
+
