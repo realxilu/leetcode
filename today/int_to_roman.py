d2d9cdf75d584f48d5cd7ff3f0b5bc2577320417
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-04 22:28:58 -0700

    t

---
+++ today/int_to_roman.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intToRoman(self, num):
+        numeric_values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        roman_numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+        res = ''
+        for val, roman in zip(numeric_values, roman_numerals):
+            # [KEY] string multiplication
+            # 0 * 'a' = ''
+            # 3 * 'a' = 'aaa'
+            res += (num // val) * roman
+            num %= val
+
+        return res
