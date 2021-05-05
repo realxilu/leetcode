b9de185630d9b28dde115d2c659e2557d6fde351
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-04 23:03:10 -0700

    Update is_same_tree.py

--- corp_Amazon/is_same_tree.py
+++ corp_Amazon/is_same_tree.py
@@ -7,3 +7,12 @@ class Solution:
             return False
 
         return p.val == q.val and self.is_same_tree(p.left, q.left) and self.is_same_tree(p.right, q.right)
+
+# 1st if
+# if both are None, both have reached the leaf nodes
+
+# 2nd if
+# if one is None and the other is not (since 1st covered both are none), then clearly aint the same tree
+
+# 1st and 2nd IFs are the exit condition for recursion
+# finally current val is the same btw the two and the relationship is recursively true for both sub nodes then the tree must be the same
