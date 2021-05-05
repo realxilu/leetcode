c9ed44eccb9f3e894a5a97a519c241ad81a5bc3c
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-04 23:11:20 -0700

    t

--- corp_Amazon/is_same_tree.py
+++ corp_Amazon/is_same_tree.py
@@ -9,10 +9,10 @@ class Solution:
         return p.val == q.val and self.is_same_tree(p.left, q.left) and self.is_same_tree(p.right, q.right)
 
 # 1st if
-# if both are None, both have reached the leaf nodes
+# if both are NULL, both have reached the leaf nodes
 
 # 2nd if
-# if one is None and the other is not (since 1st covered both are none), then clearly aint the same tree
+# if one is NULL and the other is not (since 1st check covered the case where both are none), then clearly aint the same tree
 
 # 1st and 2nd IFs are the exit condition for recursion
-# finally current val is the same btw the two and the relationship is recursively true for both sub nodes then the tree must be the same
+# finally, if current values are the same btw the two and the relationship is recursively true for both sub nodes, then the tree must be the same
--- corp_Amazon/roman_to_int.py
+++ corp_Amazon/roman_to_int.py
@@ -10,12 +10,13 @@ class Solution:
             'M': 1000
         }
 
-        string = string.replace('IV', 'IIII')
-        string = string.replace('IX', 'VIIII')
-        string = string.replace('XL', 'XXXX')
-        string = string.replace('XC', 'LXXXX')
-        string = string.replace('CD', 'CCCC')
-        string = string.replace('CM', 'DCCCC')
+        # the ordering of following doesn't matter
+        string = string.replace('IV', 'IIII')  # 4   = 5 - 1      = 1 + 1 + 1 + 1 
+        string = string.replace('IX', 'VIIII') # 9   = 10 - 1     = 5 + 1 + 1 + 1 + 1
+        string = string.replace('XL', 'XXXX')  # 40  = 50 - 10    = 10 + 10 + 10 + 10
+        string = string.replace('XC', 'LXXXX') # 90  = 100 - 10   = 50 + 10 + 10 + 10 + 10
+        string = string.replace('CD', 'CCCC')  # 400 = 500 - 100  = 100 + 100 + 100 + 100
+        string = string.replace('CM', 'DCCCC') # 900 = 1000 - 100 = 500 + 100 + 100 + 100 + 100
 
         res = 0
         for c in string:
