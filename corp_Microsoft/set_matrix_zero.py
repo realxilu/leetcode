332ed6a3037ce9315bd61c89f60f8994944603b1
Xi Lu <xilu0317@gmail.com>
2021-05-09 18:31:27 -0700

    Update set_matrix_zero.py

--- corp_Microsoft/set_matrix_zero.py
+++ corp_Microsoft/set_matrix_zero.py
@@ -2,23 +2,30 @@ class Solution:
     def setZeroes(self, matrix):
         m, n = len(matrix), len(matrix[0])
         # if the 1st col has 0, col0 will be set to 0
-        col0 = 1
+        row0, col0 = False, False
 
-        # scan through row (up -> down)
         for i in range(m):
             if matrix[i][0] == 0:
-                col0 = 0
-            # scan through col (left -> right)
+                col0 = True
+
+        for j in range(n):
+            if matrix[0][j] == 0:
+                row0 = True
+
+        for i in range(1, m):
             for j in range(1, n):
                 if matrix[i][j] == 0:
                     matrix[i][0] = matrix[0][j] = 0
 
-        # why visit in reverse order?
-        for i in reversed(range(m)):
-            for j in reversed(range(1, n)):
+        for i in range(1, m):
+            for j in range(1, n):
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
 
-            # be careful about the indentation here
-            if col0 == 0:
+        if row0:
+            for j in range(n):
+                matrix[0][j] = 0
+
+        if col0:
+            for i in range(m):
                 matrix[i][0] = 0
