dd6d437eb5ecef4103974106dca9a517efc1fe26
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-11 13:52:56 -0700

    t

--- corp_Microsoft/valid_sudoku.py
+++ corp_Microsoft/valid_sudoku.py
@@ -4,21 +4,23 @@ class Solution:
             for row in board:
                 if not is_valid(row):
                     return False
+            
             return True
 
         def is_valid_column(board):
             for col in zip(*board):
                 if not is_valid(col):
                     return False
+            
             return True
 
         def is_valid_square(board):
             for i in (0, 3, 6):
                 for j in (0, 3, 6):
-                    square = [board[x][y] for x in range(i, i+3)
-                              for y in range(j, j+3)]
+                    square = [board[x][y] for x in range(i, i+3) for y in range(j, j+3)]
                     if not is_valid(square):
                         return False
+            
             return True
 
         def is_valid(value):
--- corp_Microsoft/word_break.py
+++ corp_Microsoft/word_break.py
@@ -1,4 +1,4 @@
-# TODO: not finished
+# TODO: need to know both BFS and DP
 class Solution:
     def wordBreak(self, s, wordDict):
         pass
