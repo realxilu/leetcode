14c262c176693cde27fd5ad9d0853937abf9ca3c
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-11 13:49:36 -0700

    Update A_basics_tips.py

--- warmup/A_basics_tips.py
+++ warmup/A_basics_tips.py
@@ -1,35 +1,35 @@
-# negative number
+# NEGATIVE NUMBER
 a = 2.718
 print(-a)
 
-# fast arr reverse
+# FAST ARR REVERSE
 a = [1, 2, 3]
 print(a[::-1])
 
-# fast swap
+# FAST SWAP
 x, y = 1, 2
 x, y = y, x
 print(x, y)  # 2 1
 
-# array concat
-[1, 2] + [3, 4]
+# ARRAY CONCAT
+[1, 2] + [3, 4] + [5, 6, 7]
 
-# string concat
+# STRING CONCAT
 a = 'Hello'
 b = 'World'
 print(a + ' ' + b)
 
-# ['Onibaba', 'Onibaba', ... 'Onibaba']
+# ARRAY MULTIPLICATION
 ['Onibaba'] * 10
 
-# list comprehension
+# LIST COMPREHENSION
 ['Onibaba' for _ in range(10)]
 
-# if-else one liner
+# IF-ELSE
 age = 15
 print('kid' if age < 18 else 'adult')
 
-# array manipulation
+# ARRAY SLICING
 nums = [1, 2, 3, 4]
 start, stop = 1, 3
 nums[start:stop]  # items start through stop - 1
@@ -37,14 +37,14 @@ nums[start:]      # items start through the rest of the array
 nums[:stop]       # items from the beginning through stop - 1
 nums[:]           # nums copy of the whole array
 
+# MIXED OP
 1 + (True)  # 2
 
-# dictionary dic['stuff'] vs dic.get('stuff')
-dic = {}
-dic['a'] = 1
-dic['b'] = 2
+# MATRIX TRANSPITION
+matrix = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (10, 11, 12)]
+for row in matrix:
+    print(row)
 
-print(dic['a'])
-print(dic['b'])
-print(dic.get('c'))
-print(dic['c'])
+transposed_matrix = zip(*matrix)
+for row in transposed_matrix:
+    print(row)
