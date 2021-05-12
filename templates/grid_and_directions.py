28a7d60ae2e40348cb2009351b606fe043d7903f
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-12 00:10:20 -0700

    t

--- templates/grid_and_directions.py
+++ templates/grid_and_directions.py
@@ -1,5 +1,5 @@
 # how to typically handle grid and its moving ops:
 
 # direction [1 0] [0 1] [-1 0] [0 -1]
-rotting = {(i+di, j+dj) for i, j in rotting for di,
-           dj in [(0, 1), (1, 0), (0, -1), (-1, 0)] if (i+di, j+dj) in fresh}
+# rotting = {(i+di, j+dj) for i, j in rotting for di,
+#            dj in [(0, 1), (1, 0), (0, -1), (-1, 0)] if (i+di, j+dj) in fresh}
