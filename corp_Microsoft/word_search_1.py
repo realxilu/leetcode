314ef941aeffd2511f4dbf06118816773c61a8f9
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-13 11:29:54 -0700

    Update word_search_1.py

--- corp_Microsoft/word_search_1.py
+++ corp_Microsoft/word_search_1.py
@@ -13,17 +13,24 @@ class Solution:
 
     # check whether can find word, start at (i,j) position
     def dfs(self, board, i, j, word):
-        if len(word) == 0:  # all the characters are checked
+        # all the characters are checked
+        if len(word) == 0:
             return True
 
+        # 1) out of bound
+        # 2) word not found
         if not (0 <= i < len(board)) or not (0 <= j < len(board[0])) or word[0] != board[i][j]:
             return False
 
-        tmp = board[i][j] # first character is found, check the remaining part
-        board[i][j] = "#" # avoid visit agian
+        # first character is found, check the remaining part
+        tmp = board[i][j]
+         # avoid visit agian
+        board[i][j] = "#"
         # check whether can find "word" along one direction
         res = self.dfs(board, i + 1, j, word[1:]) or self.dfs(board, i - 1, j, word[1:]) \
             or self.dfs(board, i, j + 1, word[1:]) or self.dfs(board, i, j - 1, word[1:])
         board[i][j] = tmp
 
         return res
+
+# [KEY] 1) apply dfs on each cell
