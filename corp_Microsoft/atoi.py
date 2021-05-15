0b693fd9006cebfef81855c5d8c8eb7279559ba2
Xi Lu <xilu0317@gmail.com>
2021-05-15 00:41:19 -0700

    Create atoi.py

---
+++ corp_Microsoft/atoi.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def myAtoi(self, s):
+        digits = list(s.strip())
+
+        if len(digits) == 0:
+            return 0
+
+        sign = -1 if digits[0] == '-' else 1
+
+        if digits[0] in ('-', '+'):
+            del digits[0]
+
+        ret, i = 0, 0
+        while i < len(digits) and digits[i].isdigit():
+            ret = ret * 10 + int(digits[i])
+            i += 1
+
+        return max(-2 ** 31, min(sign * ret, 2 ** 31 - 1))
