84a737c1951c2fe0f3ac01a69f0e6867d35a771d
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-15 16:42:43 -0700

    Update meeting_rooms_2.py

--- corp_Amazon/meeting_rooms_2.py
+++ corp_Amazon/meeting_rooms_2.py
@@ -3,13 +3,17 @@ import heapq
 
 class Solution:
     def minMeetingRooms(self, intervals):
+        # sort intervals based on the starting time
         intervals.sort(key=lambda x: x[0])
 
-        # NOTE heap stores the end time of intervals
+        # NOTE heap only stores the end time of intervals
         heap = []
         for x in intervals:
+            # if the current interval's start is greater than or equal to the minimal end
+            # NOTE heap[0] is the 'peek' of the min heap of ends (thus giving us the min value), it is NOT the start of the heap
             if heap and x[0] >= heap[0]:
-                # means two intervals can use the same room
+                # then we can pop out the earlier tenants of the meeting room and push in the current tenants
+                # replace is a combined solution to pop out and push in
                 heapq.heapreplace(heap, x[1])
             else:
                 # a new room is allocated
