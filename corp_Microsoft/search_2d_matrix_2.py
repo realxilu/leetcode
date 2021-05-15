c5bfabe9aa9c4467fa9d5bb6aeb3bd1d7ea970f6
Xi Lu <xilu0317@gmail.com>
2021-05-14 23:00:20 -0700

    Create search_2d_matrix_2.py

---
+++ corp_Microsoft/search_2d_matrix_2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        i, j = 0, len(matrix[0]) - 1
+        
+        while j >= 0 and i <= len(matrix) - 1:
+            if target == matrix[i][j]:
+                return True
+            elif target < matrix[i][j]:
+                j -= 1
+            else:
+                i += 1
+
+        return False
