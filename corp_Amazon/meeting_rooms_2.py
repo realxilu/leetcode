c86dada908c84fe2eb2c9cab492e39c1f3f5a880
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-15 16:28:31 -0700

    Update meeting_rooms_2.py

--- corp_Amazon/meeting_rooms_2.py
+++ corp_Amazon/meeting_rooms_2.py
@@ -5,17 +5,17 @@ class Solution:
     def minMeetingRooms(self, intervals):
         intervals.sort(key=lambda x: x[0])
 
-        # stores the end time of intervals
+        # NOTE heap stores the end time of intervals
         heap = []
-        for interval in intervals:
-            if heap and interval[0] >= heap[0]:
+        for x in intervals:
+            if heap and x[0] >= heap[0]:
                 # means two intervals can use the same room
-                heapq.heapreplace(heap, interval[1])
+                heapq.heapreplace(heap, x[1])
             else:
                 # a new room is allocated
-                heapq.heappush(heap, interval[1])
+                heapq.heappush(heap, x[1])
 
         return len(heap)
 
-# interval[0] = start of the interval
-# interval[1] = end of the interval
+# x[0] = start of the interval
+# x[1] = end of the interval
