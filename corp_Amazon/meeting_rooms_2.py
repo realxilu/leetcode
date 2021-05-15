d1f37f403cdb1e7a9f79950f40aac2def535dd21
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-15 16:26:45 -0700

    Update meeting_rooms_2.py

--- corp_Amazon/meeting_rooms_2.py
+++ corp_Amazon/meeting_rooms_2.py
@@ -1,10 +1,12 @@
 # [PRIROITY_QUEUE]
 import heapq
+
 class Solution:
     def minMeetingRooms(self, intervals):
         intervals.sort(key=lambda x: x[0])
 
-        heap = []  # stores the end time of intervals
+        # stores the end time of intervals
+        heap = []
         for interval in intervals:
             if heap and interval[0] >= heap[0]:
                 # means two intervals can use the same room
@@ -14,3 +16,6 @@ class Solution:
                 heapq.heappush(heap, interval[1])
 
         return len(heap)
+
+# interval[0] = start of the interval
+# interval[1] = end of the interval
