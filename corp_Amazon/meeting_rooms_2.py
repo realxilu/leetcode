7997c8d1f3a5f084d8ef200bffbc1dd38ecbf365
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-15 16:57:25 -0700

    Update meeting_rooms_2.py

--- corp_Amazon/meeting_rooms_2.py
+++ corp_Amazon/meeting_rooms_2.py
@@ -6,20 +6,22 @@ class Solution:
         # sort intervals based on the starting time
         intervals.sort(key=lambda x: x[0])
 
-        # NOTE heap only stores the end time of intervals
-        heap = []
-        for x in intervals:
+        # NOTE 'rooms' only stores the *end* time of rooms
+        rooms = []
+        for cur_interval in intervals:
             # if the current interval's start is greater than or equal to the minimal end
-            # NOTE heap[0] is the 'peek' of the min heap of ends (thus giving us the min value), it is NOT the start of the heap
-            if heap and x[0] >= heap[0]:
-                # then we can pop out the earlier tenants of the meeting room and push in the current tenants
+            # roosm[0] is to 'peek' the min heap of the end time of the meeting rooms,
+            # rooms[0]: the end time for soonest-ending meeting room
+            if rooms and rooms[0] <= cur_interval[0]:
+                # then we can pop out the earlier tenants of the meeting room
+                # and push in the current tenants with its updated end time from the current interval
                 # replace is a combined solution to pop out and push in
-                heapq.heapreplace(heap, x[1])
+                heapq.heapreplace(rooms, cur_interval[1])
             else:
-                # a new room is allocated
-                heapq.heappush(heap, x[1])
+                # allocate a new room, NOTE end time of the current meeting
+                heapq.heappush(rooms, cur_interval[1])
 
-        return len(heap)
+        return len(rooms)
 
-# x[0] = start of the interval
-# x[1] = end of the interval
+# cur_interval[0] = start of the interval
+# cur_interval[1] = end of the interval
