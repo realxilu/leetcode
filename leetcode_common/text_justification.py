67cafa0eb96b2ef1902023492bf315381eabc4f5
Xi Lu <xilu0317@gmail.com>
2021-05-14 22:37:55 -0700

    t

--- leetcode_common/text_justification.py
+++ leetcode_common/text_justification.py
@@ -1,33 +1,33 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-        result, current_list, num_of_letters = [],[], 0
-        # result -> stores final result output
-        # current_list -> stores list of words which are traversed but not yet added to result
+        res, current_list, num_of_letters = [],[], 0
+        # res -> stores final res output
+        # current_list -> stores list of words which are traversed but not yet added to res
         # num_of_letters -> stores number of chars corresponding to words in current_list
         
-        for word in words:
-            # total no. of chars in current_list + total no. of chars in current word
+        for w in words:
+            # total no. of chars in current_list + total no. of chars in current w
             # + total no. of words ~= min. number of spaces between words
-            if num_of_letters + len(word) + len(current_list) > maxWidth:
+            if num_of_letters + len(w) + len(current_list) > maxWidth:
                 # size will be used for module "magic" for round robin
-                # we use max. 1 because atleast one word would be there and to avoid modulo by 0
+                # we use max. 1 because atleast one w would be there and to avoid modulo by 0
                 size = max(1, len(current_list)-1)
                 
                 for i in range(maxWidth-num_of_letters):
-                    # add space to each word in round robin fashion
+                    # add space to each w in round robin fashion
                     index = i%size
                     current_list[index] += ' ' 
                 
                 # add current line of words to the output
-                result.append("".join(current_list))
+                res.append("".join(current_list))
                 current_list, num_of_letters = [], 0
             
-            # add current word to the list and add length to char count
-            current_list.append(word)
-            num_of_letters += len(word)
+            # add current w to the list and add length to char count
+            current_list.append(w)
+            num_of_letters += len(w)
         
         # form last line by join with space and left justify to maxWidth using ljust (python method)
         # that means pad additional spaces to the right to make string length equal to maxWidth
-        result.append(" ".join(current_list).ljust(maxWidth))
+        res.append(" ".join(current_list).ljust(maxWidth))
         
-        return result
+        return res
