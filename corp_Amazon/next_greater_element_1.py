95c19dfc8a540ecc1e522f7edc3ccc85deb4e287
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-22 14:18:46 -0700

    Update next_greater_element_1.py

--- corp_Amazon/next_greater_element_1.py
+++ corp_Amazon/next_greater_element_1.py
@@ -5,6 +5,7 @@ class Solution:
         for cur_num in nums2:
             while stack and stack[-1] < cur_num:
                 dic[stack.pop()] = cur_num
+            # stack adds a path
             stack.append(cur_num)
 
         return [dic.get(x, -1) for x in nums1]
@@ -12,3 +13,6 @@ class Solution:
 # [KEY] use stack to keep track the path to the current element x
 # if the peek is smaller than the current number x
 # then peek's value's next greater element is x
+
+# stack[-1] shows the last/top element of the array/stack
+# stack[-1] is basically stack.peek() in Java
