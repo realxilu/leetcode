dcefa693538c839048bc6583d1a8e6e3c5cc644c
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-22 18:56:28 -0700

    Update largest_rectangle_in_histogram.py

--- corp_Amazon/largest_rectangle_in_histogram.py
+++ corp_Amazon/largest_rectangle_in_histogram.py
@@ -2,21 +2,60 @@ class Solution:
     def largestRectangleArea(self, height):
         height.append(0)
         stack = [-1]
-        _max = 0
+        res = 0
 
         for i in range(len(height)):
+            # as long as previous stacked heights are higher than the current height[i]
             while height[stack[-1]] > height[i]:
                 h = height[stack.pop()]
+                # NOTE this stack[-1] isn't the same as the previous stack[-1] because there is a stack.pop() on the top
                 width = i - stack[-1] - 1
-                _max = max(_max, h * width)
+                res = max(res, h * width)
             stack.append(i)
 
-        return _max
+        return res
 
 # [KEY] if previous heights are taller than the current height, keep popping them out of the stack
-# the previous heights don't have to be strictly descending as long as they are shorter than the current height
+# previous heights don't have to be strictly descending as long as they are shorter than the current height
 # the stack records previously traversed indices 
+
+
+#
+# x
+# x x 
+# x x x 
+# x x x x
+# x x x x
+#       ^
+#       |
+#       i 
+# case I: strictly descending
+
+#     x
+# x   x
+# x x x
+# x x x x
+# x x x x
+#       ^
+#       |
+#       i
+# case II: not strictly descending, but all previous items are still smaller
+
+#     x
+#     x
+#   x x
+#   x x x
+# x x x x
+# ^     ^
+# |     |
+# stop  i
+# the basic idea is to back scan from the current position 'i' to all previous bars as long as they are greater than it.
+
 
 # [KEY] we can only use the shortest height to calculate the area
 
-# [SIMILAR] to next greater element
+# [PYTHON] stack[-1] reveals the last element of the stack. This is the same as stack.peek() in Java
+
+# [SIMILAR] to corp_Amazon\next_greater_element_1.py [STACK]
+
+# [SIMILAR] [ALTERNATIVE] corp_Amazon\trap_rain_water.py
