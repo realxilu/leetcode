bee2121df4b152f3b8f449163f19b7fbb21af029
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-25 16:18:23 -0700

    Update array_partition_1.py

--- corp_Apple/array_partition_1.py
+++ corp_Apple/array_partition_1.py
@@ -4,3 +4,6 @@ class Solution:
 
 # [KEY] if a small number is grouped with a biggest number, it would waste the potential of the biggest number
 # thus sorting the input array would help
+# [1,2] [3,4] => 1 + 3
+# [1,4] [2,3] => 1 + 2
+# [1,3] [2,4] => 1 + 2
