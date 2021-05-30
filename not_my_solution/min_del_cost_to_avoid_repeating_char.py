958b26620a997013f628e7cd89df8e5bfc4f9434
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-30 01:12:09 -0700

    Update min_del_cost_to_avoid_repeating_char.py

--- not_my_solution/min_del_cost_to_avoid_repeating_char.py
+++ not_my_solution/min_del_cost_to_avoid_repeating_char.py
@@ -3,9 +3,14 @@ class Solution:
         res = max_cost = 0
         
         for i, _ in enumerate(s):
+            # if the current element (i) and its next element aren't the same, reset max_cost
             if i > 0 and s[i] != s[i - 1]:
                 max_cost = 0
             res += min(max_cost, cost[i])
             max_cost = max(max_cost, cost[i])
 
         return res
+
+# whenever we have distinct neighboring elements, the max cost will be reset to zero
+# for example, 1,2,3,4 for each step max cost will be reset
+# 1,1,1,1,1,2,2,4,5: 1...1 the max cost wont' be reset till 1,2 happens  
