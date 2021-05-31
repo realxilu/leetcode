58e5e2c5b869fc9521dc3c456aa5010e51c06031
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-30 20:15:18 -0700

    Update combination_sum.py

--- corp_Microsoft/combination_sum.py
+++ corp_Microsoft/combination_sum.py
@@ -1,14 +1,14 @@
-class Solution1:
+class Solution:
     # standard backtracking
-    def combination_sum(self, candidates, target):
+    def combinationSum(self, candidates, target):
         res = []  # needs to be referenced to be returned
-        self.dfs(candidates, target, 0, [], res)
+        self.backtrack(candidates, target, 0, [], res)
 
         return res
 
     # [KEY] need a tree diagram, things would be much easier
     # [KEY] there is a template for backtrack type of problems
-    def dfs(self, nums, target, index, path, res):
+    def backtrack(self, nums, target, index, path, res):
         if target < 0:
             return  # just backtrack here
 
@@ -17,9 +17,9 @@ class Solution1:
             return  # add to res set then backtrack
 
         for i in range(index, len(nums)):
-            self.dfs(nums, target - nums[i], i, path + [nums[i]], res)
+            self.backtrack(nums, target - nums[i], i, path + [nums[i]], res)
 
-# [KEY] 'dfs' and 'backtrack' are common names for backtrack type of problems.
+# [KEY] 'backtrack' and 'backtrack' are common names for backtrack type of problems.
 # It can be easily seen as a backtrack problem with diagrams. Say the target is 7 and it is the root and then it has different combos as branches and leaves.
 
 # 'return' is synonymous as backtrack
