1ad4f68b5a78ed111b64fde8509eb06b7e14751e
realxilu <69646469+realxilu@users.noreply.github.com>
2021-05-30 22:45:08 -0700

    Update generate_paren.py

--- corp_Microsoft/generate_paren.py
+++ corp_Microsoft/generate_paren.py
@@ -5,13 +5,20 @@ class Solution:
 
         return res
 
-    def backtrack(self, res, string, _open, close, _max):
-        if len(string) == _max * 2:
-            res.append(string)
+    def backtrack(self, res, tmp_string, _open, close, _max):
+        if len(tmp_string) == _max * 2:
+            res.append(tmp_string)
             return
 
         if _open < _max:
-            self.backtrack(res, string + '(', _open + 1, close, _max)
+            self.backtrack(res, tmp_string + '(', _open + 1, close, _max)
 
         if close < _open:
-            self.backtrack(res, string + ')', _open, close + 1, _max)
+            self.backtrack(res, tmp_string + ')', _open, close + 1, _max)
+
+# [KEY] understand what each parameters mean
+# _open: count number of open bracket
+# close: number of close bracket count
+# _max: max allowable bracket
+# tmp_string: string in the making
+# res: the shared overall result set
