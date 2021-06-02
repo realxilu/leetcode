bf8af52305cafaacde88febfba5028961c4cb2f4
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-02 00:34:26 -0700

    Update first_missing_positive.py

--- corp_Microsoft/first_missing_positive.py
+++ corp_Microsoft/first_missing_positive.py
@@ -17,3 +17,23 @@ class Solution:
 # Once I wrote out the indices and numbers, the relationship between x and i became much claerer.
 
 # TODO above is not the optimal solution
+
+
+# class Solution {
+#     public:
+#     int firstMissingPositive(vector < int > & nums) {
+#         int i = 0
+#         int n = nums.size()
+#         while (i < n)
+#         {
+#             if (nums[i] > 0 & & nums[i] <= n & & nums[nums[i] - 1] != nums[i])
+#               swap(nums[i], nums[nums[i]-1])
+#             else
+#               i++
+#         }
+#         for (i=0#              i < n#              + +i)
+    #         if (nums[i] != (i+1))
+    #           return i+1
+#         return n+1
+#     }
+# }
