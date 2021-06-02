0d7b58277ce77560a128cd06269aba738106810b
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-02 00:23:24 -0700

    Update first_missing_positive.py

--- corp_Microsoft/first_missing_positive.py
+++ corp_Microsoft/first_missing_positive.py
@@ -1,7 +1,6 @@
 class Solution:
     # O(nlogn)
     def firstMissingPositive(self, A):
-        # write your code in Python 3.6
         s = set()
         for x in A:
             if x > 0:
@@ -16,3 +15,5 @@ class Solution:
 
 # [KEY] don't be lazy, diagram it out. sometimes it is hard to think inside your head without viz
 # Once I wrote out the indices and numbers, the relationship between x and i became much claerer.
+
+# TODO above is not the optimal solution
