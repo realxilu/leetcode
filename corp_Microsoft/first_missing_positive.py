cd86b2354a9589fc80bfc3030d8e54697368616e
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-02 00:22:05 -0700

    Create first_missing_positive.py

---
+++ corp_Microsoft/first_missing_positive.py
@@ -0,0 +1,17 @@
+class Solution:
+    # O(nlogn)
+    def firstMissingPositive(self, A):
+        # write your code in Python 3.6
+        s = set()
+        for x in A:
+            if x > 0:
+                s.add(x)
+
+        i = 0
+        for i, x in enumerate(sorted(list(s))):
+            if x - i > 1:
+                return i + 1
+
+        return x + 1 if x > 0 else 1
+
+# [KEY] don't be lazy, diagram it out. sometimes it is hard to think inside your head without viz
