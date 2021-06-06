8b33bc390f3748abedad202817ea835ed15ca5ac
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-06 00:26:56 -0700

    Update verify_alien_dictionary.py

--- leetcode_less_common/verify_alien_dictionary.py
+++ leetcode_less_common/verify_alien_dictionary.py
@@ -4,6 +4,7 @@ class Solution:
         # words = [[4,2,1], [9,2,3,4,0,3], [8,2,1]]
         words = [[dic[c] for c in w] for w in words]
 
+        # [TRICK] use zip to compare the first word and second word
         for w1, w2 in zip(words, words[1:]):
             if self.is_bigger(w1, w2):
                 return False
@@ -19,3 +20,5 @@ class Solution:
             return True if s1[i] > s2[i] else False
 
         return m > n
+
+# [KEY] map 'world' => [4,3,2,1,8] using alien alphabet/order 'lrowabcdvnew'
