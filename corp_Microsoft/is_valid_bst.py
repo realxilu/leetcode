e0b44b297abfa82ad56c6039c7264e7e331f817f
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-07 11:23:35 -0700

    Update is_valid_bst.py

--- corp_Microsoft/is_valid_bst.py
+++ corp_Microsoft/is_valid_bst.py
@@ -1,10 +1,13 @@
 class Solution:
-    def is_valid_BST(self, root, MIN=float('inf'), MAX=float('-inf')):
-        if root is None:
+    def isValidBST(self, root, prev_min=float('inf'), prev_max=float('-inf')):
+        if not root:
             return True
 
-        if root.val <= MAX or root.val >= MIN:
+        if root.val <= prev_max or root.val >= prev_min:
             return False
 
-        return self.is_valid_BST(root.left,  min(MIN, root.val), MAX) and \
-               self.is_valid_BST(root.right, MIN, max(root.val, MAX))
+        cur_min = min(prev_min, root.val)
+        cur_max = max(prev_max, root.val)
+
+        return self.isValidBST(root.left,  cur_min, prev_max) and \
+               self.isValidBST(root.right, prev_min, cur_max)
