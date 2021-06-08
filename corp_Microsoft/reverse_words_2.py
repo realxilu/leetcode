1d973d2c563aba60a8c8d3648ea4e345ff256257
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-08 12:04:11 -0700

    Create reverse_words_2.py

---
+++ corp_Microsoft/reverse_words_2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseWords(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        self.reverse(s, 0, len(s) - 1)
+
+        start = 0
+        for i, _ in enumerate(s):
+            if s[i] == ' ':
+                self.reverse(s, start, i - 1)
+                start = i + 1
+
+        self.reverse(s, start, len(s) - 1)
+
+    def reverse(self, s, i, j):
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
