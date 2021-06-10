de02bfc39a61b4c441ee6df7a32831265ada3d40
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-10 12:31:37 -0700

    Update valid_palindrome.py

--- corp_Microsoft/valid_palindrome.py
+++ corp_Microsoft/valid_palindrome.py
@@ -1,7 +1,7 @@
 # [TWO-POINTER]
 class Solution:
     def isPalindrome(self, s):
-        i, j = 0, len(s)-1
+        i, j = 0, len(s) - 1
         while i < j:
             while i < j and not s[i].isalnum():
                 i += 1
