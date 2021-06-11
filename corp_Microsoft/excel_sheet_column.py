682a4621ac7b8cfd6b844026b09cabf0dc4d1924
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-11 00:35:22 -0700

    Update excel_sheet_column.py

--- corp_Microsoft/excel_sheet_column.py
+++ corp_Microsoft/excel_sheet_column.py
@@ -5,16 +5,17 @@ class Solution:
 
         res = 0
         for c in string:
-            res *= 26 # 26 base
+            res *= 26 # 26-base!
             res += ord(c) - ord('A') + 1
 
         return res
 
-# ord: convert alphabet to number
-# chr: convert number to alphabet
-
-# use similar logic but base 26
+# [KEY] use similar logic but base 26
 # 724 in base 10
 # 0 * 10 + 7 = 7
 # 7 * 10 + 2 = 72
 # 12 * 10 + 4 = 124
+
+# [PYTHON]
+# ord: convert alphabet to integer
+# chr: convert integer to alphabet
