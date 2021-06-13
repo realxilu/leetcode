254f913a001f2f2f71f02e74b055370c562bb763
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-12 23:59:48 -0700

    Update letter_combination_phone.py

--- corp_Microsoft/letter_combination_phone.py
+++ corp_Microsoft/letter_combination_phone.py
@@ -1,33 +1,34 @@
 class Solution:
+    def __init__(self):
+        self.dic = {}
+        self.dic['2'] = 'abc'
+        self.dic['3'] = 'def'
+        self.dic['4'] = 'ghi'
+        self.dic['5'] = 'jkl'
+        self.dic['6'] = 'mno'
+        self.dic['7'] = 'pqrs'
+        self.dic['8'] = 'tuv'
+        self.dic['9'] = 'wxyz'
+
     def letterCombinations(self, digits: str) -> List[str]:
         if digits == '':
             return []
 
-        res, dic = [], {}
-
-        dic['2'] = 'abc'
-        dic['3'] = 'def'
-        dic['4'] = 'ghi'
-        dic['5'] = 'jkl'
-        dic['6'] = 'mno'
-        dic['7'] = 'pqrs'
-        dic['8'] = 'tuv'
-        dic['9'] = 'wxyz'
-
-        self.backtrack('', digits, res, dic)
+        res = []
+        self.backtrack('', digits, res)
 
         return res
 
     # recusrive backtrack
-    def backtrack(self, combo, digits, res, dic):
-        # exit condition
+    def backtrack(self, combo, digits, res):
+        # exit/base condition: when running out of digits then backtrack
         if len(digits) == 0:
             res.append(combo)
             return
-        
+
         digit = digits[0]
-        letters = dic[digit]
 
         # explore other path using recursion
-        for c in letters:
-            self.backtrack(combo + c, digits[1:], res, dic)
+        # digits[1:] is a recurring pattern used to shrink the input size
+        for c in self.dic[digit]:
+            self.backtrack(combo + c, digits[1:], res)
