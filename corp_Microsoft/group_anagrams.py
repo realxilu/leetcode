5e1d29c82f15f38c409f344e0d9c924c0e5b34e5
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-13 15:46:50 -0700

    t

--- corp_Microsoft/group_anagrams.py
+++ corp_Microsoft/group_anagrams.py
@@ -1,16 +1,12 @@
-import collections
-
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if not strs:
-            return [['']]
+            return None
 
-        dic = collections.defaultdict(list)
+        dic = defaultdict(list)
         for word in strs:
-            dic[str(sorted(word))].append(word)
+            sorted_word = ''.join(sorted(list(word)))
+            dic[sorted_word].append(word)
 
-        res = []
-        for val in dic.values():
-            res.append(val)
-
-        return res
+        return list(dic.values())
