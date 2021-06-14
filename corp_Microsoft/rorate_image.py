268c6568b9daf3441fc001a6cbe1d1724a64de1c
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-13 19:03:15 -0700

    t

--- corp_Microsoft/rorate_image.py
+++ corp_Microsoft/rorate_image.py
@@ -1,21 +1,14 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        m, n = len(matrix), len(matrix[0])
-
-        if m == 0 or m == 1:
-            return matrix
-
         self._reverse(matrix)
-
-        for i in range(m):
-            for j in range(n):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
                 if i < j:
                     matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    def _reverse(self, arr):
-        i, j = 0, len(arr) - 1
-
+    def _reverse(self, a):
+        i, j = 0, len(a) - 1
         while i < j:
-            arr[i], arr[j] = arr[j], arr[i]
+            a[i], a[j] = a[j], a[i]
             i += 1
             j -= 1
