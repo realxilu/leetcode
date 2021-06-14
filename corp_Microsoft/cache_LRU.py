bf12a3b7bcdcde7d28c8b35700b3f77043d94469
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-13 19:59:28 -0700

    Update cache_LRU.py

--- corp_Microsoft/cache_LRU.py
+++ corp_Microsoft/cache_LRU.py
@@ -8,41 +8,50 @@ class LRUCache:
     def __init__(self, capacity):
         self.capacity = capacity
         self.dic = {}
-        self.head = Node(-1, -1)  # dummy
-        self.tail = Node(-1, -1)  # also dummy
+        
+        self.head = Node(-1, -1)  # senitenial
+        self.tail = Node(-1, -1)  # senitenial
+        # setup head and tail's relationshiop
         self.head.next = self.tail
         self.tail.prev = self.head
 
     def get(self, k):
         if k in self.dic:
             node = self.dic[k]
+            # this will remove the referenced node from middle
             self._remove(node)
+            # this will insert the referenced node from the back
             self._add(node)
 
             return node.v
 
-        return -1 # per spec
+        return -1 # if not found
 
     def put(self, k, v):
+        # remove-add is what guarentees LRU property
         if k in self.dic:
             self._remove(self.dic[k])
 
+        # add first then think about eviciting
         new_node = Node(k, v)
         self._add(new_node)
         self.dic[k] = new_node
 
-        # evict if length is great than 1
+        # evict if over capacity
         if len(self.dic) > self.capacity:
+            # evict items from the head, insert from behind
             head_next = self.head.next
             self._remove(head_next)
             del self.dic[head_next.k]
 
+    # node passed in as a reference for both helper function 
     def _remove(self, node):
         _prev = node.prev
         _next = node.next
         _prev.next = _next
         _next.prev = _prev
 
+    # node insertion happens from behind
     def _add(self, node):
         _prev = self.tail.prev
         _prev.next = node
