b0b7fb0c7abc2f402be1c0ea0d15473eeab240ed
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-14 22:43:39 -0700

    t

---
+++ corp_Microsoft/has_cycle_2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def detectCycle(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None
+        while head != slow:
+            slow = slow.next
+            head = head.next
+
+        return head
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
