5d069e348a8579daad2bea5707422c6baa580799
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-15 08:44:16 -0700

    Update course_schedule_1.py

--- corp_Amazon/course_schedule_1.py
+++ corp_Amazon/course_schedule_1.py
@@ -1,15 +1,17 @@
 class Solution:
     def canFinish(self, num_courses, prerequisites):
-        # [PYTHON] list comprehension
+        # ajacency-list based graph
         graph = [[] for _ in range(num_courses)]
-        # create a table
-        visited = ['new' for _ in range(num_courses)]
+        # visited status table: status NEW | VISITED | DONE
+        visited = ['NEW' for _ in range(num_courses)]
 
-        # create an ajacency list graph
+        # build graph
+        # For example, the pair[5, 8], indicates that to take course 5 you have to first take course 8. 8=>5
+        # NOTE graph[x].append(y) is semantically incorrect but it won't matter in this case
         for x, y in prerequisites:
-            graph[x].append(y)
+            graph[y].append(x)
 
-        # for each node run dfs
+        # run dfs on each node
         for x in range(num_courses):
             # if run into a visited node aka detected a cycle return false
             if self.dfs(graph, visited, x) == False:
@@ -20,15 +22,15 @@ class Solution:
     # True -> no cycle detected
     def dfs(self, graph, visited, i):
         # if ith node is marked as being visited, then a cycle is found
-        if visited[i] == 'visited':
+        if visited[i] == 'VISITED':
             return False
 
         # all nodes are visited
-        if visited[i] == 'done':
+        if visited[i] == 'DONE':
             return True
 
         # mark the current node as being visited
-        visited[i] = 'visited'
+        visited[i] = 'VISITED'
 
         # visit all the neighbors
         for x in graph[i]:
@@ -36,9 +38,8 @@ class Solution:
                 return False
 
         # after all visits, mark the node as done
-        visited[i] = 'done'
+        visited[i] = 'DONE'
 
         return True
 
-
-# For example, the pair[0, 1], indicates that to take course 0 you have to first take course 1.
+# [KEY] dfs is the key to solving this puzzle
