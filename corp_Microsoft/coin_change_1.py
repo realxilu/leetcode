8e85ce1bb180104290cfae011194f49cb799633a
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-16 22:25:40 -0700

    Update coin_change_1.py

--- corp_Microsoft/coin_change_1.py
+++ corp_Microsoft/coin_change_1.py
@@ -14,14 +14,16 @@ class Solution:
     def coinChange(self, coins, amount):
         dp = [0] + [float('inf')] * amount # [0] is the base case, it takes 0 coin to change 0 coin
 
-        # check all amounts* from $1 to 11$, build tables ground up
+        # check all amounts* from $1 to 11$, build the table ground up
         for cur_amt in range(1, amount + 1):
-            # check all coins in $1,$2,$5,$10
+            # check all coin denominations such as $1, $2, $5, $10
             for i in range(len(coins)):
-                # if current coin is less than or equal to the amount
+                # there is no point checking coin denomination greater then the current amount
                 if coins[i] <= cur_amt:
-                    # either use or not use the coin
+                    # make a decision to either use the coin or not
                     # [0, inf, inf, inf, inf, ... inf ]
+                    # NOTE [TRICKY] if the current denomiation happens to be the current amount, we use it. leading to 1 consumption
+                    # however, the way it is written in this algorithm is dp[amt-coin]+1=>dp[0]+1=>0 + 1 and the dp[cur_amt] in the next run would be 1
                     dp[cur_amt] = min(dp[cur_amt], dp[cur_amt - coins[i]] + 1)
 
         return -1 if dp[amount] > amount else dp[amount]
@@ -44,7 +46,7 @@ class Solution:
 # Output: 2
 
 class Solution:
-    # [DO_NOT_USE][GREEDY]: this ONLY works for certain coin denomiation: 1, 5, 10, 50
+    # [DO_NOT_USE][GREEDY]: this ONLY works for certain coin denomiation: 1, 5, 10, 50, but fail for 1,3,6
     def coinChange(self, coins, amount):
         coins.sort()
         count, i = 0, len(coins) - 1
