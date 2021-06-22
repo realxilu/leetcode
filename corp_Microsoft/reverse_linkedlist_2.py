779ecc9d185f3682138c7a026205e48ac8f2ec48
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-21 22:51:12 -0700

    Update reverse_linkedlist_2.py

--- corp_Microsoft/reverse_linkedlist_2.py
+++ corp_Microsoft/reverse_linkedlist_2.py
@@ -8,11 +8,13 @@ class Solution:
         dummy.next = head
         pre = dummy
 
+        # do it for m - 1 times
         for i in range(m - 1):
             pre = pre.next
 
         start = pre.next
         then = start.next
+        # draw a diagram to quickly understand the following
         for i in range(n - m):
             start.next = then.next
             then.next = pre.next
@@ -20,6 +22,11 @@ class Solution:
             then = start.next
 
         return dummy.next
+
+# 1) why use dummy
+# it's always more convinent to introduce dummy
+# 2) why stop at m - 1
+# 3) why n - m
 
 class ListNode:
     def __init__(self, val=0, next=None):
