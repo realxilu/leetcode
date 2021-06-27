639c20bade365a17d93713005f9c0275ed4daa42
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-26 23:34:15 -0700

    Update 3sum_closest.py

--- corp_Facebook/3sum_closest.py
+++ corp_Facebook/3sum_closest.py
@@ -6,16 +6,16 @@ class Solution:
         for i in range(len(num) - 2):
             j, k = i + 1, len(num) - 1
             while j < k:
-                _sum = num[i] + num[j] + num[k]
-                if _sum == target:
-                    return _sum
+                cur_sum = num[i] + num[j] + num[k]
+                if cur_sum == target:
+                    return cur_sum
 
-                if abs(_sum - target) < abs(_min - target):
-                    _min = _sum
+                if abs(cur_sum - target) < abs(_min - target):
+                    _min = cur_sum
 
-                if _sum < target:
+                if cur_sum < target:
                     j += 1
-                elif _sum > target:
+                elif cur_sum > target:
                     k -= 1
 
         return _min
