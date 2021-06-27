409b08afbc379d229c6957b122f73e066e830b6e
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-26 19:40:55 -0700

    Create right_side_view_tree.py

---
+++ corp_Facebook/right_side_view_tree.py
@@ -0,0 +1,34 @@
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return None
+
+        q, res = deque([root]), []
+        while q:
+            # maintain a level variable and the current length
+            level, cur_level_len = [], len(q)
+
+            for _ in range(cur_level_len):
+                node = q.popleft()
+                if node:
+                    # record the rightmost element
+                    level.append(node.val)
+                    # process bfs neighbors as usual only this is a binary tree
+                    q.append(node.left)
+                    q.append(node.right)
+
+            if level:
+                res.append(level[-1])
+            # reset level for it to be used in the next level
+            level = []
+
+        return res
+
+# [KEY][BFS][LEVEL-ORDER-TRAVERSAL]
+# The idea is to print out the last element of the level which by definition is the right side view
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
