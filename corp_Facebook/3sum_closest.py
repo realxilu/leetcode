6f68ccac513e05c17cd889abfad9e4cf99bb9660
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-26 23:28:37 -0700

    Create 3sum_closest.py

---
+++ corp_Facebook/3sum_closest.py
@@ -0,0 +1,21 @@
+class Solution:
+    def threeSumClosest(self, num, target):
+        num.sort()
+        _min = num[0] + num[1] + num[2]
+
+        for i in range(len(num) - 2):
+            j, k = i + 1, len(num) - 1
+            while j < k:
+                _sum = num[i] + num[j] + num[k]
+                if _sum == target:
+                    return _sum
+
+                if abs(_sum - target) < abs(_min - target):
+                    _min = _sum
+
+                if _sum < target:
+                    j += 1
+                elif _sum > target:
+                    k -= 1
+
+        return _min
