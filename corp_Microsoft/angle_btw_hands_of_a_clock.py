ac3edbaa746f1db7e63ee50bd63b7725859aa6e7
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-27 20:36:19 -0700

    t

---
+++ corp_Microsoft/angle_btw_hands_of_a_clock.py
@@ -0,0 +1,15 @@
+class Solution:
+    def angleClock(self, hour: int, minutes: int) -> float:
+
+        # the angle of hour + contribution from minutes
+        h = hour % 12 * 30 + minutes / 60 * 30
+
+        # the angle of minute
+        m = minutes * 6
+
+        angle = abs(m - h)
+
+        if angle > 180:
+            angle = 360 - angle
+
+        return angle
