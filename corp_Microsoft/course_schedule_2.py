6ba6a13571fc97e3ed239a1df5a89c5066d14ddb
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-29 11:23:18 -0700

    t

--- corp_Microsoft/course_schedule_2.py
+++ corp_Microsoft/course_schedule_2.py
@@ -12,22 +12,25 @@ class Solution(object):
             graph[y].append(x)
             in_degree[x].add(y)
 
-        # If the current node doesn't have any in_degree, then it is a sources. They don't depend on anyone now. Add them to the sources list.
-        res, sources = [], [s for s in range(numCourses) if len(in_degree[s]) == 0]
-        while sources:  # sources contains courses without prerequisites
-            new_sources = []
-            for src in sources:
+        # If the current node doesn't have any in_degree, then it is a source. They don't depend on anyone now. Add them to the srcs list.
+        res, srcs = [], [s for s in range(numCourses) if len(in_degree[s]) == 0]
+        while srcs:  # srcs contains courses without prerequisites
+            new_srcs = []
+            for src in srcs:
                 res.append(src)
-                # visit all the neighbors of the sources, since we removed the sources, we deduct the in_degree of the neightbors
+                # visit all the neighbors of the sources, since we removed the current source, we deduct the in_degree of the neightbors
                 for nbr in graph[src]:
                     in_degree[nbr].remove(src)
-                    # if the in_degree of the current node becoms zero, add it to the new sources
+                    # if the in_degree of the current node becoms zero, add it to the new srcs
                     if len(in_degree[nbr]) == 0:
-                        new_sources.append(nbr)
-            sources = new_sources  # new_sources contains new courses with no prerequisites
+                        new_srcs.append(nbr)
+            srcs = new_srcs  # new sources contains new courses with crossed-out prerequisites
 
         return res if len(res) == numCourses else []
 
 # For example, the pair [x, y], indicates that to take course x you have to first take course y.
 # That means y -> x. Be careful about ordering when building the ajacency list
 # Note that the in-degree container is just the reverse, because we are counting incoming arrows to a node.
+
+# srcs -> sources
+# nbr  -> neighbor
--- corp_Microsoft/dungeon_game.py
+++ corp_Microsoft/dungeon_game.py
@@ -1,11 +1,8 @@
-
 class Solution:
-    def calculate_minimum_HP(self, dungeon):
-        MAX = float('inf')
-
+    def calculateMinimumHP(self, dungeon):
         m, n = len(dungeon), len(dungeon[0])
 
-        dp = [[MAX] * (n + 1) for i in range(m + 1)]
+        dp = [[float('inf')] * (n + 1) for i in range(m + 1)]
 
         dp[m][n - 1], dp[m - 1][n] = 1, 1
 
@@ -16,7 +13,7 @@ class Solution:
 
         return dp[0][0]
 
-# [KEY][DP]
+# [KEY][DP] the knight needs at least 1 HP when he reaches the destination, then back out the initial hp from the origin
 # dp[i][j] = minimal HP needed at grid (i, j)
 # 1) Go from bottom right back to the origin
 # 2) To survive, the knight needs to have _at least_ 1 HP at the destination
