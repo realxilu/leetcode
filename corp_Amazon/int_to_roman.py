fec3323dd47161ac577218ea6e1c479657f11194
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-29 18:30:43 -0700

    Update int_to_roman.py

--- corp_Amazon/int_to_roman.py
+++ corp_Amazon/int_to_roman.py
@@ -1,6 +1,6 @@
 class Solution:
     def intToRoman(self, num):
-        numeric_values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        numeric_values = [1000, 900, 500, 400,  100,  90,   50,  40,  10,    9,   5,    4,    1]
         roman_numerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
         res = ''
@@ -10,5 +10,18 @@ class Solution:
 
         return res
 
-# NOTE: 0 * 'a' = ''
+# [IMPORTANT] 0 * 'a' = ''
 # 3 * 'a' = 'aaa'
+
+# 743 // 1000, 743 // 900, 743 // 500 = 1, ....
+# D
+# 743 % 500 = 243
+# 243 // 1000, ... 243 // 100 = 2, ...
+# DCC
+# ... 243 % 100 = 43 ...
+# 43 // 1000, ... 43 // 40 = 1, ...
+# DCCXL
+# 43 % 40 = 3
+# ... 3 // 1 = 3
+# DCCXLIII
+
