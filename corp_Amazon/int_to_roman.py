10e925332c76fec4aa1b22b4e0ffc5bfefe8752b
realxilu <69646469+realxilu@users.noreply.github.com>
2021-06-29 18:48:32 -0700

    Update int_to_roman.py

--- corp_Amazon/int_to_roman.py
+++ corp_Amazon/int_to_roman.py
@@ -1,10 +1,10 @@
 class Solution:
     def intToRoman(self, num):
-        numeric_values = [1000, 900, 500, 400,  100,  90,   50,  40,  10,    9,   5,    4,    1]
-        roman_numerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+        digits = [1000, 900, 500, 400,  100,  90,   50,  40,  10,    9,   5,    4,    1]
+        romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
 
         res = ''
-        for digit, roman in zip(numeric_values, roman_numerals):
+        for digit, roman in zip(digits, romans):
             res += (num // digit) * roman
             num %= digit
 
