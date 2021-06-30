dbda1d5bd6a5cf5b8b163bb58631d7818762de5d
Xi Lu <xilu0317@gmail.com>
2021-06-29 23:53:53 -0700

    Update 3sum_lc15.py

--- corp_Amazon/3sum_lc15.py
+++ corp_Amazon/3sum_lc15.py
@@ -6,6 +6,7 @@ class Solution:
         for i in range(len(nums) - 2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
+            
             l, r = i + 1, len(nums) - 1
             while l < r:
                 _sum = nums[i] + nums[l] + nums[r]
@@ -15,6 +16,7 @@ class Solution:
                     r -= 1
                 else:
                     res.append((nums[i], nums[l], nums[r]))
+                    # handle duplicates
                     while l < r and nums[l] == nums[l + 1]:
                         l += 1
                     while l < r and nums[r] == nums[r - 1]:
