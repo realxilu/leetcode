fa195fda250b5976d392fabab7c656413b06fb94
realxilu <69646469+realxilu@users.noreply.github.com>
2021-07-02 11:51:21 -0700

    Update valid_paren.py

--- corp_Other_companies/valid_paren.py
+++ corp_Other_companies/valid_paren.py
@@ -1,7 +1,6 @@
 class Solution:
     def isValid(self, s):
         stack = []
-
         for x in s:
             if x == '(':
                 stack.append(')')
@@ -9,9 +8,22 @@ class Solution:
                 stack.append(']')
             elif x == '{':
                 stack.append('}')
-            elif len(stack) == 0 or stack.pop() != x:
+            # 'not stack' covers test case 1
+            elif not stack or stack.pop() != x:
                 return False
 
         return len(stack) == 0
 
-# there is no need for map using this strategy
+
+# Note we don't need hashmap here, just push the closing paren into the stack
+# TEST CASES:
+# 1. ))))
+# [] -> the stack is empty prematurely, the stack shouldn't be empty at this point, return false
+# 2. ()) 
+# [)] -> [] -> the stack is empty again like case 1
+# 3. (((
+#  [)] -> [)] -> [)], len(stack) is 3 in the end, return false
+# 4. (()
+# [)] -> [))] -> [)], len(stack) is 1 in the end, return false
+# 5. (()())
+# [)] -> [))] -> [)] -> [))] -> [)] -> [], len(stack) is 0 in the end, return true
